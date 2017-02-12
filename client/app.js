bfa46c646b0aa125d917af5edfa25f688f368aca
amran16 <maryamg99@gmail.com>
2017-02-12 10:18:10 -0800

    fix merge conflict

--- client/app.js
+++ client/app.js
@@ -3,9 +3,30 @@ function handleSubmit() {
 
 	axios.post('/api/gift', { user: document.getElementById('input').value }).then(response => {
 
+		var data = response.data;
+
+		// var display = data.map(function(item){
+		//
+		// 	 var image = item.imageURI;
+		// 	 var name =  item.caption;
+		// 	 var description = item.the_Description;
+		//
+		// 	 console.log(name + ': ' + description);
+		//
+		// });
+
+		$('#display').append('<li><a href=' + image + '><h3>' + name + '</h3></a>' + description + '</p></li>');
+
+		//$('#display').append('<li><a href=' + data[3][i] +'><h2>' + data[1][i] +'</h2></a>' +'<p>' + data[2][i] +
+					//'</p></li>');
+
 		// if this happens, response is the data from the shop api:
 		// -> render production suggestions to UI
+
 		console.log("Response", response);
+
+		//console.log(response);
+
 
 	}).catch(err => {
 
@@ -14,7 +35,7 @@ function handleSubmit() {
 		console.log('error!');
 
 	});
-	
+
 	return false;
 
 };
