8cd354c725e09dac0c9b4093da09d1051eb42a61
Sean Smith <sean.smith.2009@gmail.com>
2017-02-11 20:32:09 -0800

    resolved conflicts

--- client/app.js
+++ client/app.js
@@ -1,9 +1,20 @@
 
-console.log('js');
-
 function handleSubmit() {
-	axios.post('/api/gift').then(response => {
+
+	axios.post('/api/gift', { user: document.getElementById('input').value }).then(response => {
+
+		// if this happens, response is the data from the shop api:
+		// -> render production suggestions to UI
 		console.log(response);
+
+	}).catch(err => {
+
+		// if this happens, no instagram user was found:
+		// -> render message to UI:
+		console.log('error!');
+
 	});
+	
 	return false;
-}
+
+};
