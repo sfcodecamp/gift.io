135f057b1b0c28b943380720732aca915f3e10b3
Shav <shavenchew@yahoo.com>
2017-02-12 11:12:22 -0800

    Added ScrollReveal on the client

--- client/app.js
+++ client/app.js
@@ -1,6 +1,24 @@
 
 function handleSubmit() {
 
+	$('#input').blur();
+	$('.stackable').html('');
+	$('.flex-container').append(`
+		<div class="ui segment">
+			<div class="ui active dimmer">
+				<div class="ui text loader">Loading</div>
+			</div>
+			<p></p>
+		</div>
+		`);
+
+			window.sr = ScrollReveal();
+
+	    sr.reveal('.card', {
+	      duration: 1000,
+	      viewFactor: 0.2,
+	    }, 300);
+
 	axios.post('/api/gift', { user: document.getElementById('input').value }).then(response => {
 
 		var data = response.data;
@@ -10,10 +28,22 @@ function handleSubmit() {
 			 var image = item.imageURI;
 			 var name =  item.caption;
 			 var description = item.the_Description;
+			 var linkUrl  	 = item.linkUrl;
 
 			 console.log(name + ': ' + description);
 
-			 $('#display').append('<li><a href=' + image + '><h3>' + name + '</h3></a>' + description + '</p></li>');
+
+			 $('.stackable').append(`
+				  <a class="ui card" href=${name}linkUrl>
+				 	 <div class="image">
+				 		 <img src=${image} alt="Grizzly Fitness Black Grizzly Paw Training Gloves - XXL">
+				 	 </div>
+				 	 <div class="content">
+				 		 <div class="header">${name}</div>
+				 		 <div class="description">${description}</div>
+				 	 </div>
+				  </a>
+				 `);
 
 		});
 
@@ -31,7 +61,7 @@ function handleSubmit() {
 		// -> render message to UI:
 		console.log('error!');
 
-		$('#display').append('<h2>No instagram user was found</h2>');
+		$('.stackable').append('<h2>No instagram user was found</h2>');
 
 	});
 
